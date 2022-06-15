bc813033d2892e9ce7aa7d1e7be7e55708cfd295
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-06-15 17:51:21 -0400

    added burgershot doorlocks

---
+++ configs/burgershot.lua
@@ -0,0 +1,71 @@
+
+
+-- burgershot1 created by Doug Heffernan
+Config.DoorList['burgershot-burgershot1'] = {
+    objCoords = vec3(-1179.314819, -891.477661, 13.865158),
+    fixText = false,
+    objName = -1635579193,
+    authorizedJobs = { ['burgershot'] = 0 },
+    doorType = 'door',
+    doorRate = 1.0,
+    objYaw = 124.19993591309,
+    distance = 2,
+    locked = true,
+    doorLabel = 'whitedoor',
+}
+
+-- burgershot2 created by Doug Heffernan
+Config.DoorList['burgershot-burgershot2'] = {
+    doors = {
+        {objName = 386432549, objYaw = 304.19998168945, objCoords = vec3(-1183.274536, -885.629517, 14.169803)},
+        {objName = -806752263, objYaw = 304.19998168945, objCoords = vec3(-1184.722290, -883.499268, 14.169803)}
+    },
+    doorRate = 1.0,
+    distance = 2,
+    doorLabel = 'maindoors',
+    authorizedJobs = { ['burgershot'] = 0 },
+    locked = true,
+    doorType = 'double',
+}
+
+-- burgershot3 created by Doug Heffernan
+Config.DoorList['burgershot-burgershot3'] = {
+    doors = {
+        {objName = 386432549, objYaw = 34.19995880127, objCoords = vec3(-1196.755127, -883.576965, 14.169803)},
+        {objName = -806752263, objYaw = 34.19995880127, objCoords = vec3(-1198.884644, -885.024048, 14.169803)}
+    },
+    doorRate = 1.0,
+    distance = 2,
+    doorLabel = 'sidedoors',
+    authorizedJobs = { ['burgershot'] = 0 },
+    locked = true,
+    doorType = 'double',
+}
+
+-- burgershot4 created by Doug Heffernan
+Config.DoorList['burgershot-burgershot4'] = {
+    doors = {
+        {objName = 1980817304, objYaw = 34.19995880127, objCoords = vec3(-1202.487427, -896.648926, 14.123632)},
+        {objName = 1980817304, objYaw = 214.19996643066, objCoords = vec3(-1204.187988, -897.804626, 14.123632)}
+    },
+    doorRate = 1.0,
+    distance = 2,
+    doorLabel = 'kitchendoors',
+    authorizedJobs = { ['burgershot'] = 0 },
+    locked = true,
+    doorType = 'double',
+}
+
+-- burgershot5 created by Doug Heffernan
+Config.DoorList['burgershot-burgershot5'] = {
+    objCoords = vec3(-1193.732666, -900.285034, 14.123634),
+    fixText = false,
+    objName = 1462909834,
+    authorizedJobs = { ['burgershot'] = 0 },
+    doorType = 'door',
+    doorRate = 1.0,
+    objYaw = 304.19998168945,
+    distance = 2,
+    locked = true,
+    doorLabel = 'registerdoor',
+}
