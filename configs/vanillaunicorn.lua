5931c18076534eb4374bdcab1672d66252b44d09
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-06-15 17:50:51 -0400

    added vanilla unicorn door locks

---
+++ configs/vanillaunicorn.lua
@@ -0,0 +1,85 @@
+
+
+-- vanilla1 created by Doug Heffernan
+Config.DoorList['vanillaunicorn-vanilla1'] = {
+    objCoords = vec3(127.950066, -1298.506592, 29.419622),
+    authorizedJobs = { ['vanilla'] = 0 },
+    locked = true,
+    doorType = 'door',
+    fixText = false,
+    distance = 2,
+    doorRate = 1.0,
+    doorLabel = 'mainentrance',
+    objYaw = 29.999988555908,
+    objName = -1116041313,
+}
+
+-- vanilla2 created by Doug Heffernan
+Config.DoorList['vanillaunicorn-vanilla2'] = {
+    objCoords = vec3(128.070801, -1279.346680, 29.436968),
+    authorizedJobs = { ['vanilla'] = 0 },
+    locked = true,
+    doorType = 'door',
+    fixText = false,
+    distance = 2,
+    doorRate = 1.0,
+    doorLabel = 'djbooth',
+    objYaw = 210.0,
+    objName = 1695461688,
+}
+
+-- vanilla3 created by Doug Heffernan
+Config.DoorList['vanillaunicorn-vanilla3'] = {
+    objCoords = vec3(113.410095, -1296.260010, 29.435989),
+    authorizedJobs = { ['vanilla'] = 0 },
+    locked = true,
+    doorType = 'door',
+    fixText = false,
+    distance = 2,
+    doorRate = 1.0,
+    doorLabel = 'backstage',
+    objYaw = 300.0,
+    objName = 390840000,
+}
+
+-- vanilla4 created by Doug Heffernan
+Config.DoorList['vanillaunicorn-vanilla4'] = {
+    objCoords = vec3(99.083069, -1293.689087, 29.440405),
+    authorizedJobs = { ['vanilla'] = 0 },
+    locked = true,
+    doorType = 'door',
+    fixText = false,
+    distance = 2,
+    doorRate = 1.0,
+    doorLabel = 'office',
+    objYaw = 29.999988555908,
+    objName = 390840000,
+}
+
+-- vanilla5 created by Doug Heffernan
+Config.DoorList['vanillaunicorn-vanilla5'] = {
+    objCoords = vec3(96.091972, -1284.853760, 29.438784),
+    authorizedJobs = { ['vanilla'] = 0 },
+    locked = true,
+    doorType = 'door',
+    fixText = false,
+    distance = 2,
+    doorRate = 1.0,
+    doorLabel = 'backentrance',
+    objYaw = 210.0,
+    objName = 1695461688,
+}
+
+-- vanilla6 created by Doug Heffernan
+Config.DoorList['vanillaunicorn-vanilla6'] = {
+    objCoords = vec3(116.227760, -1294.592529, 29.435989),
+    authorizedJobs = { ['vanilla'] = 0 },
+    locked = true,
+    doorType = 'door',
+    fixText = false,
+    distance = 2,
+    doorRate = 1.0,
+    doorLabel = 'premiumlounge',
+    objYaw = 300.0,
+    objName = 390840000,
+}
