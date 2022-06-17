e328263840e8db66840aadc02d500e542e209feb
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-06-17 18:21:23 -0400

    added tequilala doorlocks

---
+++ configs/tequilala.lua
@@ -0,0 +1,43 @@
+
+
+-- tequilala1 created by Doug Heffernan
+Config.DoorList['tequilala-tequilala1'] = {
+    authorizedJobs = { ['tequilala'] = 0 },
+    locked = true,
+    distance = 2,
+    doorRate = 1.0,
+    objName = -2037125726,
+    objYaw = 355.00003051758,
+    fixText = false,
+    objCoords = vec3(-563.887329, 276.516632, 83.286263),
+    doorType = 'door',
+    doorLabel = 'mainentrance',
+}
+
+-- tequilala2 created by Doug Heffernan
+Config.DoorList['tequilala-tequilala2'] = {
+    authorizedJobs = { ['tequilala'] = 0 },
+    locked = true,
+    distance = 2,
+    doorRate = 1.0,
+    objName = 1927676967,
+    objYaw = 355.00003051758,
+    fixText = false,
+    objCoords = vec3(-567.583252, 280.997650, 83.126434),
+    doorType = 'door',
+    doorLabel = 'frontdesk',
+}
+
+-- tequilala3 created by Doug Heffernan
+Config.DoorList['tequilala-tequilala3'] = {
+    authorizedJobs = { ['tequilala'] = 0 },
+    locked = true,
+    distance = 2,
+    doorRate = 1.0,
+    objName = 1927676967,
+    objYaw = 265.00006103516,
+    fixText = false,
+    objCoords = vec3(-560.350647, 291.715027, 82.326088),
+    doorType = 'door',
+    doorLabel = 'vip',
+}
