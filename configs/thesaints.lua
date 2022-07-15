ad205e50af955d71523ab0658b8e1d5b8fa98fd1
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-07-14 22:37:51 -0400

    added thesaints doorlock file

---
+++ configs/thesaints.lua
@@ -0,0 +1,57 @@
+
+
+-- thesaints1 created by Brandin Martinez
+Config.DoorList['thesaints-thesaints1'] = {
+    doorType = 'double',
+    doors = {
+        {objName = 159994461, objYaw = 291.00006103516, objCoords = vec3(-816.716003, 179.097961, 72.827377)},
+        {objName = -1686014385, objYaw = 291.00006103516, objCoords = vec3(-816.106812, 177.510864, 72.827377)}
+    },
+    distance = 2,
+    authorizedGangs = { ['thesaints'] = 0 },
+    locked = true,
+    doorLabel = 'main door',
+    doorRate = 1.0,
+}
+
+-- thesaints2 created by Brandin Martinez
+Config.DoorList['thesaints-thesaints2'] = {
+    doorType = 'garage',
+    objName = 30769481,
+    locked = true,
+    objCoords = vec3(-815.281616, 185.974991, 72.999931),
+    fixText = false,
+    doorLabel = 'garage',
+    authorizedGangs = { ['thesaints'] = 0 },
+    doorRate = 1.0,
+    distance = 2,
+    objYaw = 291.00012207031,
+}
+
+-- thesaints3 created by Brandin Martinez
+Config.DoorList['thesaints-thesaints3'] = {
+    doorType = 'double',
+    doors = {
+        {objName = 1245831483, objYaw = 21.000057220459, objCoords = vec3(-794.505127, 178.012375, 73.040451)},
+        {objName = -1454760130, objYaw = 21.000057220459, objCoords = vec3(-796.565674, 177.221375, 73.040451)}
+    },
+    distance = 2,
+    authorizedGangs = { ['thesaints'] = 0 },
+    locked = true,
+    doorLabel = 'back door1',
+    doorRate = 1.0,
+}
+
+-- thesaints4 created by Brandin Martinez
+Config.DoorList['thesaints-thesaints4'] = {
+    doorType = 'double',
+    doors = {
+        {objName = -1454760130, objYaw = 111.00005340576, objCoords = vec3(-793.394348, 180.507462, 73.040451)},
+        {objName = 1245831483, objYaw = 111.00005340576, objCoords = vec3(-794.185303, 182.568008, 73.040451)}
+    },
+    distance = 2,
+    authorizedGangs = { ['thesaints'] = 0 },
+    locked = true,
+    doorLabel = 'backdoor',
+    doorRate = 1.0,
+}
