8dbddbb6edfef576faf63df3d5a7d352f3ef774c
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-09-05 19:56:38 -0400

    added unknowndemons doorlocks

---
+++ configs/unknowndemons.lua
@@ -0,0 +1,57 @@
+
+
+-- demons1 created by Doug
+Config.DoorList['unknowndemons-demons1'] = {
+    locked = true,
+    doorLabel = 'frontgate',
+    authorizedGangs = { ['demons'] = 0 },
+    distance = 3,
+    doorType = 'double',
+    doors = {
+        {objName = -1249591818, objYaw = 336.21197509766, objCoords = vec3(-132.788544, 971.500244, 236.114319)},
+        {objName = 546378757, objYaw = 336.21197509766, objCoords = vec3(-137.799057, 973.708862, 236.114319)}
+    },
+    doorRate = 1.0,
+}
+
+-- demons2 created by Doug
+Config.DoorList['unknowndemons-demons2'] = {
+    locked = true,
+    doorLabel = 'frontdoor',
+    fixText = false,
+    authorizedGangs = { ['demons'] = 0 },
+    objYaw = 110.21258544922,
+    objCoords = vec3(-112.618622, 986.785034, 235.914627),
+    distance = 3,
+    doorType = 'door',
+    objName = -658026477,
+    doorRate = 1.0,
+}
+
+-- demons3 created by Doug
+Config.DoorList['unknowndemons-demons3'] = {
+    locked = true,
+    doorLabel = 'grarageright',
+    fixText = false,
+    authorizedGangs = { ['demons'] = 0 },
+    objYaw = 19.371480941772,
+    objCoords = vec3(-125.082520, 1011.371826, 236.505997),
+    distance = 3,
+    doorType = 'garage',
+    objName = 1307087556,
+    doorRate = 1.0,
+}
+
+-- demons4 created by Doug
+Config.DoorList['unknowndemons-demons4'] = {
+    locked = true,
+    doorLabel = 'grarageleft',
+    fixText = false,
+    authorizedGangs = { ['demons'] = 0 },
+    objYaw = 19.371480941772,
+    objCoords = vec3(-131.922348, 1008.952515, 236.505997),
+    distance = 3,
+    doorType = 'garage',
+    objName = 1307087556,
+    doorRate = 1.0,
+}
